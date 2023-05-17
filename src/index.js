29c5453d119645ff6993bdb91887e7b2008c8db2
santty04 <sarango844@misena.edu.co>
2023-05-17 18:33:24 -0500

    Change the text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Seguimos con el curso avanzado!</h1>, document.getElementById('app'))
+ReactDOM.render(<h1>Deployed!</h1>, document.getElementById('app'))
